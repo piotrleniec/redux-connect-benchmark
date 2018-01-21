e1130b5f52eb56274e616fb1e775f0ef893f4d46
Piotr Leniec <leniec.piotr@gmail.com>
2018-01-21 13:41:25 +0100

    added and configured react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import store from './store'
 import NumberInput from './components/NumberInput'
+import A from './components/A'
+import B from './components/B'
+import C from './components/C'
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,17 +13,19 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+        <BrowserRouter>
+          <div>
+            <ul>
+              <li><Link to="/a">a</Link></li>
+              <li><Link to="/b">b</Link></li>
+              <li><Link to="/c">c</Link></li>
+            </ul>
 
-          <NumberInput />
-        </div>
+            <Route exact path="/a" component={A} />
+            <Route exact path="/b" component={B} />
+            <Route exact path="/c" component={C} />
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
---
+++ src/components/A.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NumberInput from './NumberInput'
+
+export default () => (
+  <div>
+    <h1>This is A</h1>
+
+    <NumberInput />
+  </div>
+)
